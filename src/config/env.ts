3365a918f517a9e4f697c4581507fba096f11e42
Mobinul Islam Mahi <mobinulislam143>
2026-04-21 08:41:55 +0600

    port change

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 5070,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY!,
   SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL!,
   AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY!,
